cd11e060859f20e87d79cba945fbfffef26ec7e9
mtoribio <marcos.toribio.farmy@gmail.com>
2019-03-26 18:04:17 +0100

    added filter params in url added only view func to add new filters

--- app/controllers/chartl/charts_controller.rb
+++ app/controllers/chartl/charts_controller.rb
@@ -3,12 +3,14 @@ module Chartl
     # [GET]
     def show
       load_chart
-      
+      chartl_arguments
+      filter
+
       response.headers['X-Robots-Tag'] = 'noindex, nofollow'
 
       if request.format == 'csv'
         separator = params[:csv_separator] || ","
-        render text: CSV.generate(col_sep: separator) { |csv| @chart.as_csv.each { |r| csv << r }}
+        render text: CSV.generate(col_sep: separator) {|csv| @chart.as_csv.each {|r| csv << r}}
       else
         render_show
       end
@@ -46,5 +48,42 @@ module Chartl
         render :cohort
       end
     end
+
+    def chartl_arguments
+      header_chart = @chart.series.first['data'].first
+      ary_params = params.permit(header_chart.map(&:to_sym)).to_h
+      @chartl_arguments = ((header_chart & ary_params.keys).map {|k| [k, ary_params[k]]}.to_h).map {|j, v| j == 'completed_at' ? [j, JSON.parse(v)] : [j, v]}.to_h || {}
+    end
+
+    def filter
+      data = @chart.series.first['data']
+      headers = data.first
+      string_selector = ''
+      if @chartl_arguments
+        @chartl_arguments.each do |k, v|
+          string_selector += " && " if @chartl_arguments.keys.first != k
+          if v.is_a?(Array)
+            string_selector += "DateTime.parse(k[#{headers.find_index(k)}]) >= DateTime.parse('#{v[0]}') && DateTime.parse(k[#{headers.find_index(k)}]) <= DateTime.parse('#{v[1]}')"
+          else
+            string_selector += "k[#{headers.find_index(k)}].to_s == '#{v}'"
+          end
+        end
+
+        @chart.series.first['data'] = data.select.with_index do |k, index|
+          index == 0 ? true : eval(string_selector)
+        end
+      end
+    end
   end
 end
+
+# TODO
+# Nueva columna para guardar la busqueda (jsonb)
+#
+# Show:
+# * Comprobamos se pasan nuevos parametros:
+#    - Si: Guardamos los nuevos parametros y se recalculan los datos
+#    - No: Nada
+# * Se muestra la grafica los datos guardados
+#
+# ?range_from=2019.01.01&range_to=2019.02.01&id=50799
